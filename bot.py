bfa24dfccfdd1783d7dc754a3c030de49d80d180
Matthiasclee <matthias@matthiasclee.com>
2022-02-20 10:56:17 -0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,45 @@
+import discord
+import shlex
+from discord.ext.commands import has_permissions
+import requests
+import json
+from os.path import exists
+
+print("Starting client...")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    global g_id
+    global guild
+
+    print("Bot ready!")
+
+    for guild in client.guilds:
+        print(guild.name + ": " + str(guild.id))
+
+    g_id = input("Which guild do you want to join? ")
+    guild = discord.utils.find(lambda g : g.id == int(g_id), client.guilds)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if str(message.guild.id) != g_id:
+        return
+
+    bot_text = ""
+
+    if message.author.bot:
+        bot_text = " [bot]"
+    
+    color = ""
+    
+    for mention in message.mentions:
+        if mention.id == client.user.id:
+            color = "\033[43m"
+
+    print(color + "#" + message.channel.name + " - " + message.author.name + "#" + str(message.author.discriminator) + bot_text + ": " + message.content + "\033[0m")
+
+client.run(open('token','r').read())

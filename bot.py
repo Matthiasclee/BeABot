5ff4250a9cd031aecda6250ee3ebf1bc5617f17f
Matthiasclee <matthias@matthiasclee.com>
2022-02-20 11:00:05 -0800

    Initial commit

--- bot.py
+++ bot.py
@@ -36,10 +36,13 @@ async def on_message(message):
     
     color = ""
     
+    msgcontent = message.content
+
     for mention in message.mentions:
+        msgcontent = msgcontent.replace("<@!" + str(mention.id) + ">", "@" + mention.name + "#" + mention.discriminator)
         if mention.id == client.user.id:
             color = "\033[43m"
 
-    print(color + "#" + message.channel.name + " - " + message.author.name + "#" + str(message.author.discriminator) + bot_text + ": " + message.content + "\033[0m")
+    print(color + "#" + message.channel.name + " - " + message.author.name + "#" + str(message.author.discriminator) + bot_text + ": " + msgcontent + "\033[0m")
 
 client.run(open('token','r').read())

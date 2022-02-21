c671ce3fa55944bb17978da0a5a1bf30dc2aa791
Matthiasclee <matthias@matthiasclee.com>
2022-02-21 09:14:31 -0800

    Misc.

--- read.py
+++ read.py
@@ -11,24 +11,10 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    global g_id
-    global guild
-
     print("Bot ready!")
-
-    for guild in client.guilds:
-        print(guild.name + ": " + str(guild.id))
-
-    g_id = input("Which guild do you want to join? ")
-    guild = discord.utils.find(lambda g : g.id == int(g_id), client.guilds)
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    if str(message.guild.id) != g_id:
-        return
-
     bot_text = ""
 
     if message.author.bot:
@@ -46,6 +32,6 @@ async def on_message(message):
         if mention.id == client.user.id:
             color = "\033[43m"
 
-    print(color + "#" + message.channel.name + " - " + message.author.name + "#" + str(message.author.discriminator) + bot_text + ": " + msgcontent + "\033[0m")
+    print(color + "(" + message.guild.name + " " + "#" + message.channel.name + ") " + message.author.name + "#" + str(message.author.discriminator) + bot_text + ": " + msgcontent + "\033[0m")
 
 client.run(open('token','r').read())
